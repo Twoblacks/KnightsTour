405402f1b98cccfc5bd04ebc30b4b1884f55f233
Twoblacks <adrian.227664@nv.ccsd.net>
2022-05-13 08:38:53 -0700

    Added comments

--- Knights Tour/src/Main.java
+++ Knights Tour/src/Main.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.*;
 public class Main {
+    //Checks if there's only one tile with value -1
     public static boolean ifLast(Tile[][] board){
         int n = 0;
         for (int i = 0; i < 8; i++){
@@ -14,34 +15,45 @@ public class Main {
         return (n==1);
     }
 
+    // Method for finding how many secondary moves there are for a given tile and board
+    // returns the tile of the next move
     public static Tile search(Tile pos, Tile[][] board) {
         Tile temp = new Tile();
         int count = 0;
         int low = 8;
+
+        // Directions knight can move in y and x axis
         int[] row = { 2, 2, 1, -1, -2, -2, 1, -1};
         int[] col = {-1, 1, 2,  2, -1,  1,-2, -2};
+
+        // Checks if there's only one open space or not
         if(!(ifLast(board))) {
+            // Checks how many primary moves the knight can take
             for (int i = 0; i < 8; i++) {
                 count = 0;
                 try {
                     if (board[pos.getRow() + row[i]][pos.getCollumn() + col[i]].getTaken() == -1) {
+                        // For each primary move check valid secondary moves,
+                        // and increment count for each valid move
                         for (int j = 0; j < 8; j++) {
                             try {
                                 if (board[pos.getRow() + row[i] + row[j]][pos.getCollumn() + col[i] + col[j]].getTaken() == -1) {
                                     count++;
                                 }
-                            } catch (ArrayIndexOutOfBoundsException e) {
-                            }
+                            } catch (ArrayIndexOutOfBoundsException e) {}
                         }
                     }
-                } catch (ArrayIndexOutOfBoundsException e) {
-                }
+                } catch (ArrayIndexOutOfBoundsException e) {}
+                // - Checks if the amount of secondary moves for one of the primary moves is less than the current lowest
+                // - sets that to be current lowest, and sets the temp tile be the tile at the index of the primary move
+                //   with the lowest secondary moves
                 if (count < low && count >= 1) {
                     low = count;
                     temp = new Tile(pos.getRow() + row[i], pos.getCollumn() + col[i], -1);
                 }
             }
         } else {
+            // If there's only one tile remaining sets temp to be that tile
             for (int i = 0; i < 8; i++) {
                 try {
                     if (board[pos.getRow() + row[i]][pos.getCollumn() + col[i]].getTaken() == -1) {
@@ -54,28 +66,33 @@ public class Main {
     }
 
     public static void main(String args[]) {
+        // Scanner and inputs
         Scanner s = new Scanner(System.in);
         System.out.println("Row: ");
         int rowP = s.nextInt();
         System.out.println("Col: ");
         int colP = s.nextInt();
 
+        // Board set-up
         Tile[][] board = new Tile[8][8];
         for(int row = 0; row<8; row++){
             for(int col = 0; col<8; col++){
                 board[row][col] = new Tile(row, col, -1);
             }
         }
+
         // Setting Start Position
         board[rowP][colP].setTaken(0);
         Tile l = new Tile(rowP, colP, -1);
 
+        // Solves Knights Tour
         System.out.println();
         for(int i = 1; i <= 63; i++){
             l = search(l, board);
             board[l.getRow()][l.getCollumn()].setTaken(i);
         }
 
+        // Prints the board
         for(int r = 0; r < 8; r++){
             for(int c = 0; c < 8; c++){
                 System.out.print(board[r][c]+" ");
@@ -84,6 +101,6 @@ public class Main {
         }
 
         // Testing Search
-        //.out.println(search(l, board));
+        //System.out.println(search(l, board));
     }
 }
--- Knights Tour/src/Tile.java
+++ Knights Tour/src/Tile.java
@@ -1,8 +1,11 @@
+// Tile class primarily for holding row/column position,
+// and number representing either an empty space or its incremented nubmer on the board.
 public class Tile {
     private int row;
     private int collumn;
     private int taken;
 
+    // Tile constructors
     public Tile(int row, int collumn, int taken){
         this.row = row;
         this.collumn = collumn;
@@ -11,20 +14,19 @@ public class Tile {
     public Tile(){
     }
 
+    // Data getters
     public int getRow(){
         return row;
     }
-
     public int getCollumn() {
         return collumn;
     }
-
     public int getTaken() { return taken; }
+
+    // Number setter
     public void setTaken(int num) { taken = num; }
+
+    // Two toStrings one for debugging, one for solution
     //public String toString() {return "(" + row + " " + collumn+ ")";}
     public String toString() {return "" + taken;}
-
-    public boolean equalTo(Tile til){
-        return(row == til.getRow() && collumn == til.getCollumn());
-    }
 }
